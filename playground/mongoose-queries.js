aff74350bc872406a7d8f34eb36e6b9d9c36e582
shalini1995 <34934102+shalini1995@users.noreply.github.com>
2019-02-06 17:26:06 +0530

    find queries

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,18 @@
+const {ObjectID} = require('mongodb');
+
+var {mongoose} = require('./../server/db/mongoose.js');
+var {User} = require('./../server/models/user.js');
+
+
+var id = '5c5831609bd46128703f3552';
+
+if(!ObjectID.isValid(id)) {
+  console.log('Id is not valid');
+}
+
+
+User.findById(id).then((user)=>{
+    console.log(user.email);
+  }).catch((e)=>{
+    console.log(e);
+  });
